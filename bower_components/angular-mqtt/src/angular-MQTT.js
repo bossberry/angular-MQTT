e50517c06d52a6d86251c14ec26b046ba58f8bde
bossberry <32351225+bossberry@users.noreply.github.com>
2018-06-05 15:39:38 +0700

    Update angular-MQTT.js

--- bower_components/angular-mqtt/src/angular-MQTT.js
+++ bower_components/angular-mqtt/src/angular-MQTT.js
@@ -1,5 +1,6 @@
 /**
- * Created by shellus on 2016-03-16. Update by bossberry
+ * Created by shellus on 2016-03-16.
+ * Edit by Tanakun wangpong on 2018-06-05.
  */
 angular.module('ngMQTT', [])
     .config(['$provide', function($provide) {
@@ -20,46 +21,44 @@ angular.module('ngMQTT', [])
       });
     }])
 
-    .service('MQTTService', ['$q', '$rootScope', function($q, $rootScope) {
+    .service('MQTTService', ['MQTT', '$state', '$q', '$rootScope', function(MQTT, $state, $q, $rootScope) {
       var Service = {};
       var callbacks = {};
-      var client;
-      Service.connect = function(vhref, voption) {
-        client = mqtt.connect(vhref, voption); // you add a ws:// url here
-        client.on('message', function(topic, payload) {
-          try {
-            var data = JSON.parse(payload.toString());
-          } catch (e) {
-            var data = payload.toString();
-          }
-          angular.forEach(callbacks, function(callback, name) {
-            var regexpStr = name.replace(new RegExp('(#)|(\\*)|(\\+)'), function(str) {
-              switch (str) {
-                case '#':
-                  return '.*?';
-                  break;
-                case '*':
-                  return '.*?';
-                  break;
-                case '+':
-                  return '.*';
-                  break;
-                default:
-                  break;
-              }
-            });
-            if (topic.match(regexpStr)) {
-              $rootScope.$apply(function() {
-                callback(data, topic);
-              });
+      var client = mqtt.connect(MQTT.href, MQTT.option);
+      client.on('message', function(topic, payload) {
+        try {
+          var data = JSON.parse(payload.toString());
+        } catch (e) {
+          var data = payload.toString();
+        }
+        angular.forEach(callbacks, function(callback, name) {
+          var regexpStr = name.replace(new RegExp('(#)|(\\*)|(\\+)'), function(str) {
+            switch (str) {
+              case '#':
+                return '.*?';
+                break;
+              case '*':
+                return '.*?';
+                break;
+              case '+':
+                return '.*';
+                break;
+              default:
+                break;
             }
           });
+          if (topic.match(regexpStr)) {
+            $rootScope.$apply(function() {
+              callback(data, topic);
+            });
+          }
         });
-
-        client.publish('time', (new Date()).getDate());
-
-      };
-
+      });
+      client.on('error', function(data) {
+        window.location.href = '/api/auth/signout';
+        $state.go('authentication.signin');
+      });
+      client.publish('time', (new Date()).getDate());
       Service.on = function(name, callback) {
         callbacks[name] = callback;
         client.subscribe(name);
